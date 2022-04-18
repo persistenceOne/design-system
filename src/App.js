8b5f9afadd9f15f23682883994a9835b3cce8c65
Raju Vemula <rajuvemula@Rajus-MacBook-Air.local>
2022-04-18 08:58:19 +0530

    build update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import "./scss/main.css"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <button className="button-primary">butotn</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>
+         <button className="button button-primary">button primary</button> &nbsp; &nbsp;
+         <button className="button button-secondary ">button secondary</button>&nbsp; &nbsp;
+         <button className="button button-tertiary">button-tertiary</button>
+       </div>
       </header>
     </div>
   );
---
+++ src/examples/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Examples = () =>{
+    return(
+        <p>dd</p>
+    );
+}
+export default Examples;
