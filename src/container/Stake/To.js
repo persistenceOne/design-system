bfcbf46203571c998e2c707636698002f477d4e4
Raju Vemula <rajuvemula@Rajus-MacBook-Air.local>
2022-05-02 12:58:58 +0530

    update input styles

--- src/container/Stake/To.js
+++ src/container/Stake/To.js
@@ -4,10 +4,13 @@ import stkXprt from "../../assets/images/stk_xprt.svg";
 const To = () => {
     return (
         <div className="input-field-container flex-fill">
-            <div className="dropdown-input swap-next">
-                <div className="input-logo">
-                    <img src={stkXprt} className="logo" alt="cc"/>
-                    stkXPRT
+            <div className="logo-input swap-next">
+                <div className="input-logo-section">
+                    <div className="input-logo">
+                        <img src={stkXprt} className="logo" alt="cc"/>
+                        stkXPRT
+                    </div>
+                    <p className="available"><span className="text">Available: </span> <span className="value">0.00</span> </p>
                 </div>
                 <div className="input-section">
                     <input
